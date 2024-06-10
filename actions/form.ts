e2d5fb3621d68794bd1546b2116f578b2e413847
Dat_PH <datph@mor.com.vn>
2024-06-10 15:19:59 +0700

    add source

---
+++ actions/form.ts
@@ -0,0 +1,34 @@
+"use server"
+
+import { currentUser } from "@clerk/nextjs/server"
+import prisma from "../lib/prisma";
+class UserNotFoundError extends Error { }
+export async function getFormStats() {
+    const user = await currentUser();
+
+    if (!user) {
+        throw new UserNotFoundError()
+    }
+
+    const stats = prisma.form.aggregate({
+        where: {
+            userId: user.id
+        },
+        _sum: {
+            visits: true,
+            submissions: true
+        }
+    })
+
+    const visits = (await stats)._sum.visits || 0
+    const submissions = (await stats)._sum.submissions || 0
+    let submissionRate = 0
+
+    if (visits > 0) {
+        submissionRate = (submissions / visits) * 100
+    }
+
+    const bounceRate = 100 - submissionRate
+
+    return { visits, submissions, submissionRate, bounceRate }
+}
---
+++ components/ThemeSwitcher.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { DesktopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+function ThemeSwitcher() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList className="border">
+        <TabsTrigger value="light" onClick={() => setTheme("light")}>
+          <SunIcon className="h-[1.2rem w-[1.2rem]"></SunIcon>
+        </TabsTrigger>
+        <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
+          <MoonIcon className="h-[1.2rem w-[1.2rem] rotate-90 transition-none dark:rotate-0"></MoonIcon>
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+}
+
+export default ThemeSwitcher;
